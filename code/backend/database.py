f97aad9ca9fde207239b0151a3f15a6e124aa8bb
jordan-v22 <jordan.vieira@wsu.edu>
2025-11-30 21:25:01 -0800

    database persistence test

--- code/backend/database.py
+++ code/backend/database.py
@@ -3,6 +3,8 @@ database.py
 Handles SQLite database connection and queries.
 
 TEAM OWNER: Database 1 (Profiles) & Database 2 (Matches) (Emery)
+
+**Line addition to test DB persistence**
 """
 
 import os
